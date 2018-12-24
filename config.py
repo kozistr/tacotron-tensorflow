4d48bed4f17a61b97c282026ae9330122c5825d7
kozistr <kozistr@gmail.com>
2018-12-24 17:50:39 +0900

    update: configuration

--- config.py
+++ config.py
@@ -22,25 +22,35 @@ def get_config():
 
 # Network
 network_arg = add_arg_group('Network')
-network_arg.add_argument('--mode', type=str, default='non-static', choices=['static', 'non-static'])
-network_arg.add_argument('--model', type=str, default='charcnn', choices=['charcnn', 'charrnn'])
-network_arg.add_argument('--filter_size', type=int, default=256,
-                         help='conv1d filter size')
+network_arg.add_argument('--model', type=str)
+network_arg.add_argument('--n_encoder_banks', type=int, default=16)
+network_arg.add_argument('--n_decoder_banks', type=int, default=8)
+network_arg.add_argument('--n_highway_blocks', type=int, default=4)
+network_arg.add_argument('--reduction_factor', type=int, default=5)
+network_arg.add_argument('--filter_size', type=int, default=256)
 network_arg.add_argument('--fc_unit', type=int, default=1024)
-network_arg.add_argument('--drop_out', type=float, default=.7)
+network_arg.add_argument('--dropout', type=float, default=.5)
 
 
 # DataSet
 data_arg = add_arg_group('DataSet')
-data_arg.add_argument('--embed_size', type=int, default=300,
+data_arg.add_argument('--sample_rate', type=int, default=22050,
+                      help="sample rate of an audio")
+data_arg.add_argument('--n_fft', type=int, default=2048,
+                      help="fft points (samples)")
+data_arg.add_argument('--frame_shift', type=float, default=0.0125)
+data_arg.add_argument('--frame_length', type=float, default=0.05)
+data_arg.add_argument('--n_mels', type=int, default=80)
+data_arg.add_argument('--power', type=float, default=1.2,
+                      help="Exponent for amplifying the predicted magnitude")
+data_arg.add_argument('--n_iters', type=int, default=50)
+data_arg.add_argument('--preemphasis', type=float, default=.97)
+data_arg.add_argument('--max_db', type=float, default=100.)
+data_arg.add_argument('--min_db', type=float, default=20.)
+data_arg.add_argument('--embed_size', type=int, default=256,
                       help='the size of character/word embedding vector')
-data_arg.add_argument('--vocab_size', type=int, default=391587,
-                      help='default is w2v vocab size')
-data_arg.add_argument('--character_size', type=int, default=251,
-                      help='number of korean chars')
-data_arg.add_argument('--sequence_length', type=int, default=400,
-                      help='the length of the sentence, default is c2v max words cnt')
-data_arg.add_argument('--batch_size', type=int, default=128)
+data_arg.add_argument('--vocab_size', type=int, default=391587)
+data_arg.add_argument('--batch_size', type=int, default=32)
 
 
 # Train/Test hyper-parameters
@@ -51,7 +61,7 @@ train_arg.add_argument('--logging_step', type=int, default=500)
 train_arg.add_argument('--optimizer', type=str, default='adam', choices=['adam', 'sgd', 'adadelta'])
 train_arg.add_argument('--l2_reg', type=float, default=5e-4)
 train_arg.add_argument('--grad_clip', type=float, default=5.)
-train_arg.add_argument('--lr', type=float, default=2e-4)
+train_arg.add_argument('--lr', type=float, default=1e-3)
 train_arg.add_argument('--lr_decay', type=float, default=.75)
 train_arg.add_argument('--lr_lower_boundary', type=float, default=2e-5)
 train_arg.add_argument('--test_size', type=float, default=.2)
